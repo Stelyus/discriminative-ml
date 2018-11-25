c145c9c88d32abf70b1b1ced2e08555404d4e8ea
Franck Thang <stelyus@outlook.com>
2018-11-26 00:59:30 +0100

    Logistic regression

---
+++ logistic_regression/log_reg.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import pickle
+import numpy
+import sys
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import train_test_split
+
+
+## Logistic regression
+
+def plot_points(xy, labels):
+    for i, label in enumerate(set(labels)):
+        points = np.array([xy[j,:] for j in range(len(xy)) if labels[j] == label])
+        marker = MARKERS[i % len(MARKERS)]
+        color = COLORS[i % len(COLORS)]
+        plt.scatter(points[:,0], points[:,1], marker=marker, color=color)
+    plt.show()
+
+
+# x.shape 10,2
+# w.shape 2 1
+def predict(w, x):
+    z = x @ w
+    sigmoid  = 1  / (1 + np.exp(- z))
+    return sigmoid
+
+# Cross entropy
+def cost_function(w,x,y):
+    cost = 0
+    for sample in range(x.shape[0]):
+        hx,  ys = predict(w,  x[sample,:]), y[sample]
+        cost += ys * np.log(hx) + (1-ys) * np.log(1 - hx)
+    return - 1/x.shape[0] *  cost
+
+def gradient_descent(x, y, lr=.01):
+    ones = np.ones((x.shape[0], x.shape[1] -1))
+    x = np.concatenate((x,ones),axis=-1)
+    w = np.zeros((x.shape[1],1))
+    y = y.reshape(-1,1)
+    epoch = 1000000
+
+    for i in range(epoch):
+        cost = cost_function(w,x,y)
+        print("Error function {}".format(cost))
+
+        N = x.shape[0] 
+        pred = predict(w,x)
+        gradient = np.dot(x.T, pred - y)
+
+        gradient /= N
+        gradient *= lr
+        w -= gradient
+
+
+    # Prediction 
+    prediction = np.vectorize(lambda x: 1 if x < .5 else 0)(predict(w,x))
+    succ  = 0
+    for i in range(prediction.shape[0]):
+        print(prediction[i])
+        print(y[i][0])
+        succ += prediction[i] == y[i][0]
+    print("Acc {}/{}".format(succ, x.shape[0]))
+
+
+
+
+data = pd.read_csv('data.txt')
+
+reduced = data[data['class'] <= 2]
+X = reduced.as_matrix(columns=['alcohol', 'ash'])
+y = label_binarize(reduced['class'].values, [1, 2])[:,0]
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.25, random_state = 42)
+
+
+# Plotting
+MARKERS = ['+', 'x', '.']
+COLORS = ['red', 'green', 'blue']
+
+
+# ytrain = np.vectorize(lambda x: {0:-1,1:1}[x])(ytrain)
+gradient_descent(Xtrain, ytrain)
--- svm/svm.py
+++ svm/svm.py
@@ -7,28 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import label_binarize
 from sklearn.model_selection import train_test_split
 
-# Write code here
-data = pd.read_csv('data.txt')
 
-reduced = data[data['class'] <= 2]
-X = reduced.as_matrix(columns=['alcohol', 'ash'])
-y = label_binarize(reduced['class'].values, [1, 2])[:,0]
-
-print("y shape {}".format(y.shape))
-print("X shape{}".format(X.shape))
-
-
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.25, random_state = 42)
-Xtrain = Xtrain[:10,:]
-ytrain = ytrain[:10]
-
-#print('train:', len(Xtrain), 'test:', len(Xtest))
-
-
-
-# Plotting
-MARKERS = ['+', 'x', '.']
-COLORS = ['red', 'green', 'blue']
+## HARD MARGIN
 
 def plot_points(xy, labels):
     for i, label in enumerate(set(labels)):
@@ -40,32 +20,56 @@ def plot_points(xy, labels):
 
 #plot_points(Xtrain[:10,:], ytrain[:10])
 
+def acc(w, b, x, y):
+    succ  = 0
+    for sample in range(x.shape[0]):
+        succ += y[sample][0] == np.sign(np.dot(w, x[sample,:]) - b)
+    return succ / x.shape[0]
+    
 def gradient_descent(x, y, lr=.001):
     y = y.reshape(-1,1)
-    w = np.zeros((x.shape[1]))
+    w = np.random.uniform(0, 1, (x.shape[1]))
     alpha = np.zeros(y.shape)
-    b = 0
+    b = np.random.uniform(-1,1,1)
+    EPOCH = 1_000
 
     # Computing the derivatives
-    for _ in range(100):
-        dw = w - np.sum((alpha @ y.T) @ x)
-        db = - np.sum(alpha @ y.T)
-        dalpha = - np.sum(y.T @ ((x @ w) + b).reshape(-1,1) - 1)
+    for _ in range(EPOCH):
+        print("Accuracy {}".format(acc(w,b,x,y))) 
+        
+        '''
+        for sample in range(x.shape[0]):
+            val = y[sample] * (np.dot(x[sample,:], w) - b)
+            if val < 1:
+                w = w + lr * (y[sample] * x[sample,:] -2 * 1/EPOCH * w)
+            else:
+                w = w + lr * -2 * w * 1/EPOCH
+        '''
 
-        new_w = w - lr * dw
-        new_b = b - lr * db
-        new_alpha = alpha - lr * dalpha
-
-        w, b, alpha = new_w, new_b, new_alpha
-
+    '''
     count, success = 0, 0
     for i in range(x.shape[0]):
-        success += int(np.sign(np.dot(x[i,:], w) + b)) == y[i][0]
+        success += int(np.sign(np.dot(x[i,:], w) - b)) == y[i][0]
         count += 1
 
     print("Succ: {}/{}".format(success, count))
+    '''
+
+
+data = pd.read_csv('data.txt')
+
+reduced = data[data['class'] <= 2]
+X = reduced.as_matrix(columns=['alcohol', 'ash'])
+y = label_binarize(reduced['class'].values, [1, 2])[:,0]
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.25, random_state = 42)
+Xtrain = Xtrain[:10,:]
+ytrain = ytrain[:10]
+
+# Plotting
+MARKERS = ['+', 'x', '.']
+COLORS = ['red', 'green', 'blue']
 
 
 ytrain = np.vectorize(lambda x: {0:-1,1:1}[x])(ytrain)
-
 gradient_descent(Xtrain, ytrain)
