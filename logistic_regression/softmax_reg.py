ca31ba53c283e2fbe366fc9ac8108905dde81f6b
Franck Thang <stelyus@outlook.com>
2018-11-27 20:58:04 +0100

    First version of gradient descent for softmax regression, not working

--- logistic_regression/softmax_reg.py
+++ logistic_regression/softmax_reg.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import label_binarize
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
+np.seterr(all='print')
+
 '''
 Softmax regression / Multinomial logistic regression
 
@@ -20,8 +22,7 @@ COLORS = ['red', 'green', 'blue']
 
 
 '''
-    xi.shape (3,)
-'''  
+Not used anymore
 def cost_function(x,y,w,K):
     cost, N = 0, x.shape[0]
     for sample in range(N):
@@ -32,51 +33,58 @@ def cost_function(x,y,w,K):
         v = np.sum(np.exp(w @ xi))
         cost += np.log(u / v)
     return - 1/N * cost
+'''
 
 
 
 def cost_refractor(x,y,w,K):
     n = x.shape[0] 
-    # value = softmax  shape (133,3)
+    
+    print(np.max(x))
+    print(np.min(x))
+    print(w)
     u = np.exp(x @ w.T)
     v = np.sum(u, axis=-1).reshape(-1,1)
+    
     softmax = u / v
+    softmax += 0.001
+    if np.isnan(np.log(softmax)).any():
+        print("Found it")
+        sys.exit()
     return - 1/n * np.sum(np.diag(np.log(softmax) @ y.T))
 
 # Gradient descent
-def softmax_reg(x,y,K,lr=.01):
-    EPOCH = 100
+def softmax_reg(x,y,K,lr=.01,precision=.01):
+    EPOCH = 1000
     N = x.shape[0]
     ones = np.ones((N, 1))
     x = np.concatenate([x, ones], axis=-1)
+    i = 1
+    # delta = precision + 1
     
-
     '''
-    n_class x n_weights
-    w.shape (3,3)
+            n_class x n_weights
+            w.shape (3,3)
     '''
-    
     w = np.random.uniform(-1,1, (K, x.shape[1]))
 
-    for _ in range(EPOCH):
-        cost = cost_function(x,y,w,K)
-        cost1 = cost_function_refractor(x,y,w,K)
-
-        print("Error cost: {0:.2f}".format(cost_refractor))
-        gradient_dj = 0
+    while i < EPOCH:
+        cost = cost_refractor(x,y,w,K)
+        print("Iteration {0}, error cost: {1:.2f}".format(i, cost))
+        u = np.exp(x @ w.T)
+        v = np.sum(u, axis=-1).reshape(-1,1)
+        # softmax (133,3)
+        softmax = u / v
         
-        '''
-            yi, xi = y[sample,:], x[sample,:]
-            yi = yi.reshape(-1,1) 
-            theta_t = w.T @ yi
-            u = np.exp(theta_t.T @ xi)
-            v = np.sum(np.exp(w @ xi))
-            np.log(u / v)
-        '''
+        # p (133,3)
+        p = y - softmax
         
-        w = w - lr * gradient_dj
-        sys.exit()
-
+        #res (133,133)
+        res = x @ p.T
+        
+        gradient = -1/N * np.sum(np.diag(res))
+        w = w - lr * gradient
+        i += 1 
     return w
    
 
@@ -105,6 +113,7 @@ print("ytrain shape {}".format(ytrain.shape))
 
 print("Xtest shape {}".format(Xtest.shape))
 print("ytest shape {}".format(ytest.shape))
+print("-" * 15)
 
 #plot_points(Xtrain, ytrain.argmax(axis=1))
 softmax_reg(Xtrain, ytrain, K)
