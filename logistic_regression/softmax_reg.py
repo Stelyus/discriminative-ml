dd06c67719df629736c21b9503f1b45d097475f8
Franck Thang <stelyus@outlook.com>
2018-11-27 15:46:40 +0100

    Optimize cost function for softmax regression

--- logistic_regression/softmax_reg.py
+++ logistic_regression/softmax_reg.py
@@ -19,35 +19,66 @@ MARKERS = ['+', 'x', '.']
 COLORS = ['red', 'green', 'blue']
 
 
-
-
 '''
-    n_class x n_sample
-    w.shape (3,3)
     xi.shape (3,)
 '''  
-
 def cost_function(x,y,w,K):
-    cost = 0
-    for sample in range(x.shape[0]):
+    cost, N = 0, x.shape[0]
+    for sample in range(N):
         yi, xi = y[sample,:], x[sample,:]
         yi = yi.reshape(-1,1) 
         theta_t = w.T @ yi
         u = np.exp(theta_t.T @ xi)
         v = np.sum(np.exp(w @ xi))
         cost += np.log(u / v)
-    return - cost
-            
+    return - 1/N * cost
 
 
 
-def softmax_reg(x,y,K):
-    ones = np.ones((x.shape[0], 1))
+def cost_refractor(x,y,w,K):
+    n = x.shape[0] 
+    # value = softmax  shape (133,3)
+    u = np.exp(x @ w.T)
+    v = np.sum(u, axis=-1).reshape(-1,1)
+    softmax = u / v
+    return - 1/n * np.sum(np.diag(np.log(softmax) @ y.T))
+
+# Gradient descent
+def softmax_reg(x,y,K,lr=.01):
+    EPOCH = 100
+    N = x.shape[0]
+    ones = np.ones((N, 1))
     x = np.concatenate([x, ones], axis=-1)
-    w = np.random.uniform(-1,1, (K, x.shape[1]))
     
-    cost = cost_function(x,y,w,K)
-    print("Error: {}".format(cost))
+
+    '''
+    n_class x n_weights
+    w.shape (3,3)
+    '''
+    
+    w = np.random.uniform(-1,1, (K, x.shape[1]))
+
+    for _ in range(EPOCH):
+        cost = cost_function(x,y,w,K)
+        cost1 = cost_function_refractor(x,y,w,K)
+
+        print("Error cost: {0:.2f}".format(cost_refractor))
+        gradient_dj = 0
+        
+        '''
+            yi, xi = y[sample,:], x[sample,:]
+            yi = yi.reshape(-1,1) 
+            theta_t = w.T @ yi
+            u = np.exp(theta_t.T @ xi)
+            v = np.sum(np.exp(w @ xi))
+            np.log(u / v)
+        '''
+        
+        w = w - lr * gradient_dj
+        sys.exit()
+
+    return w
+   
 
 
 
@@ -59,12 +90,15 @@ def plot_points(xy, labels):
         plt.scatter(points[:,0], points[:,1], marker=marker, color=color)
     plt.show()
 
+
+# Number of class
 K = 3
+
 data = pd.read_csv('data.txt')
 X = data.as_matrix(columns=['alcohol', 'flavanoids'])
 y = data.as_matrix(columns=['class'])
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.25)
-ytrain = label_binarize(ytrain, range(1,K+1))
+ytrain = label_binarize(ytrain, range(1, K+1))
 
 print("Xtrain shape {}".format(Xtrain.shape))
 print("ytrain shape {}".format(ytrain.shape))
@@ -72,6 +106,5 @@ print("ytrain shape {}".format(ytrain.shape))
 print("Xtest shape {}".format(Xtest.shape))
 print("ytest shape {}".format(ytest.shape))
 
-# Number of class
 #plot_points(Xtrain, ytrain.argmax(axis=1))
 softmax_reg(Xtrain, ytrain, K)
