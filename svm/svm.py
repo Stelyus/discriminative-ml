4c9ddd09f0bd46d73cc480cde859fdcfdadfde94
Franck Thang <stelyus@outlook.com>
2018-12-07 14:53:21 +0100

    Created animation for svm

--- svm/svm.py
+++ svm/svm.py
@@ -1,10 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from sklearn.datasets.samples_generator import make_blobs
 
+
+'''
+References:
+    http://www.robots.ox.ac.uk/~az/lectures/ml/lect2.pdf
+'''
+
+
 X, y = make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=40)
-
-
 X1 = np.c_[np.ones((X.shape[0])), X] 
 
 ''' 
@@ -12,40 +18,66 @@ X1 = np.c_[np.ones((X.shape[0])), X]
    y   (50,)
 '''
 def perceptron_algorithm(x, y):
-    y = np.vectorize(lambda x: 1 if x == 1 else -1)(y)
     y = y.reshape(-1,1) # 50,1
-    # w = np.zeros((x.shape[1],1)) # 3,1
+    points, ret = [], []
+    
+    # Must init w with random and not with zero
+    # The scalar product will be 0 and np.sign is 0. Therefore no weight update
     w = np.random.uniform(-1,1,(x.shape[1],1))
-    n = x.shape[0]
-    epoch = 100
-    for _ in range(epoch):
+    n, lr, nb_epoch = x.shape[0], .01, 100
+    for epoch in range(nb_epoch):
         for i in range(n):
-            res = np.dot(w.reshape(3,), x[i])
+            res = np.dot(w.reshape(3,),x[i])
             if np.sign(res) != np.sign(y[i]):
-                w = w + .02 * np.sign(res) * res
-        predict(x,y,w)
-    return w
+                ret.append(w)
+                points.append(i)
+                w = w + lr * np.sign(res) * res
+            if epoch % 100 == 0:
+                predict(x,y,w)
+    return ret, points
+
+
+def animation_perceptron(x,y):
+    def animate(iteration):
+        plt.clf()
+        step = .01
+        w = ret[iteration].ravel()
+        i = points[iteration]
+        pt = x[i]
+        res = np.dot(w.reshape(3,),x[i])
+        print("w {}".format(w))
+        print("sign y {}, sign res {}".format(np.sign(y[i]), np.sign(res)))
+        print("new weights: {}".format(w + .01 * np.sign(res) * res))
+        
+        xmax, xmin = 11, -5
+        ymax, ymin = 5, -13
+        
+        xx, yy = np.meshgrid(np.arange(xmin,xmax,step), np.arange(ymin,ymax,step))
+        xy = np.c_[xx.ravel(), yy.ravel()]
+        xy = np.c_[np.ones(xy.shape[0]), xy]
+        res = np.sign((xy @ w).reshape(xx.shape))
+        
+        cont = plt.contour(xx,yy,res,colors='black')
+        plt.quiver(0,0,w[1],w[2])
+        plt.scatter(x[:,1], x[:,2], marker='o', c=y)
+        plt.scatter(pt[1], pt[2],c='red')
+        
+        plt.axis([-5,10,-12,5])
+        return cont
+    
+    fig = plt.figure()
+    ret, points = perceptron_algorithm(x,y)
+    ani = animation.FuncAnimation(fig, animate, frames=len(ret),
+                                  blit=False, interval=200, repeat=False)
+    plt.show()
+    return ret[-1]
 
 def predict(x,y,w):
-    res = x @ w
-    score = np.count_nonzero(np.sign(res) == np.sign(y))
-    print("Score {}".format(score / res.shape[0]))
+    correct_pred = np.count_nonzero(np.sign(x @ w) == np.sign(y))
+    accuracy  = correct_pred / x.shape[0]
+    ("Accuracy {}%".format(accuracy))
 
+y = np.vectorize(lambda x: -1 if x == 1 else 1)(y)
+w = animation_perceptron(X1,y)
+# plot(X1,y,w)
 
-def plot(x,y,w):
-    step = .01
-    xmax, ymax = x[:,1].max() - 1, x[:,2].max() + 1
-    xmin, ymin = x[:,1].min() - 1, x[:,2].min() - 1
-    
-    xx, yy = np.meshgrid(np.arange(xmin,xmax,step), np.arange(ymin,ymax,step))
-    xy = np.c_[xx.ravel(), yy.ravel()]
-    xy = np.c_[np.ones(xy.shape[0]), xy]
-    # xy shape (1163052, 3)
-    res = np.sign((xy @ w).reshape(xx.shape))
-    plt.contour(xx,yy,res,colors='black')
-    plt.scatter(X1[:,1], X1[:,2], marker='o', c=y)
-    plt.axis([-5,10,-12,-1])
-    plt.show()
-
-w = perceptron_algorithm(X1, y)
-plot(X1,y,w)
